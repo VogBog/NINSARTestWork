13554ac0c37584071786bdffed696b38486839de
VogBog <vogbog92@gmail.com>
2025-06-03 00:29:10 +0300

    fix: file havent white spaces

--- Assets/Scripts/FileReader.cs
+++ Assets/Scripts/FileReader.cs
@@ -14,7 +14,7 @@ public class FileReader
             throw new NullReferenceException("Cannot find resources file");
 
         var text = textAsset.text;
-        var lines = text.Split('\n');
+        var lines = text.Split(Environment.NewLine);
         if(lines.Length == 0 || lines[0].Length == 0)
             throw new NullReferenceException("File is empty");
 
@@ -27,9 +27,9 @@ public class FileReader
         if (!File.Exists(PersistentFilePath))
         {
             File.Create(PersistentFilePath).Close();
-            File.WriteAllText(PersistentFilePath, "1 1 1\n2 2 2\n3 3 3\n");
+            File.WriteAllText(PersistentFilePath, "111\n222\n333\n");
         }
-        
+
         string[] lines = File.ReadAllLines(PersistentFilePath);
 
         if (lines.Length == 0 || lines[0].Length == 0)
@@ -43,7 +43,7 @@ public class FileReader
 
     private int[,] Parse(string[] lines)
     {
-        int width = lines[0].Split(' ').Length;
+        int width = lines[0].Length;
         int height = 0;
         foreach (var line in lines)
         {
@@ -56,18 +56,16 @@ public class FileReader
 
         for (int i = 0; i < lines.Length; i++)
         {
-            string[] strNumber = lines[i].Split(' ');
-            
-            if (strNumber.Length != width)
+            if (lines[i].Length != width)
             {
                 throw new Exception("Array is not a matrix");
             }
             
-            for (int j = 0; j < strNumber.Length; j++)
+            for (int j = 0; j < lines[i].Length; j++)
             {
-                if (!int.TryParse(strNumber[j], out int number))
+                if (!int.TryParse(lines[i][j].ToString(), out int number))
                 {
-                    throw new Exception($"Cannot parse the number {strNumber[j]}");
+                    throw new Exception($"Cannot parse the number {lines[i][j]} at indexes {i} {j}");
                 }
                 data[i, j] = number;
             }
